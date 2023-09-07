2b2e0d780e6fa9b741b758afbcad4bc6da1c8695
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-09-07 19:09:58 +0300

    complete places viewmodel

---
+++ app/src/main/java/com/androiddevtaha/touristspotter/viewmodel/PlacesViewModel.kt
@@ -0,0 +1,54 @@
+package com.androiddevtaha.touristspotter.viewmodel
+
+import BaseUiEffect
+import BaseViewModel
+import com.androiddevtaha.touristspotter.entites.places.Feature
+import com.androiddevtaha.touristspotter.usecase.GetPlacesUseCase
+import com.androiddevtaha.touristspotter.viewmodel.base.ErrorUiState
+import com.androiddevtaha.touristspotter.viewmodel.uiStates.SearchListUiState
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+class PlacesViewModel @Inject constructor(
+    private val getPlaces: GetPlacesUseCase
+) : BaseViewModel<SearchListUiState, BaseUiEffect>(SearchListUiState()) {
+
+    private fun fetchNextPage(
+        lonMin: Double,
+        lonMax: Double,
+        latMin: Double,
+        latMax: Double,
+        kinds: String,
+        limit: Int = 20
+    ) {
+        _state.update { SearchListUiState(isLoading = true, error = null) }
+        tryToExecute(
+            execute = { getPlaces.invoke(lonMin, lonMax, latMin, latMax, kinds, limit) },
+            onSuccessWithData = ::onSuccess,
+            onError = ::onError,
+        )
+    }
+
+    private fun onSuccess(places: List<Feature>) {
+        _state.update {
+            SearchListUiState(
+                places = places,
+                isLoading = false,
+                error = null,
+            )
+        }
+    }
+
+    private fun onError(error: ErrorUiState) {
+        _state.update {
+            SearchListUiState(
+                places = emptyList(),
+                isLoading = false,
+                error = error,
+            )
+        }
+    }
+
+
+}
+
