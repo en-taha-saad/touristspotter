1532ef0e85d02756dbd5d1aacc968033ca273385
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-09-07 16:49:08 +0300

    complete places repository interface and implementation

--- app/src/main/java/com/androiddevtaha/touristspotter/repository/PlacesRepositoryImpl.kt
+++ app/src/main/java/com/androiddevtaha/touristspotter/repository/PlacesRepositoryImpl.kt
@@ -1,6 +1,41 @@
 package com.androiddevtaha.touristspotter.repository
 
+import com.androiddevtaha.touristspotter.entites.placeDetails.PlaceDetails
+import com.androiddevtaha.touristspotter.entites.places.Places
+import com.androiddevtaha.touristspotter.repository.mapper.toEntity
 import com.androiddevtaha.touristspotter.usecase.PlacesRepository
+import javax.inject.Inject
 
-class PlacesRepositoryImpl : PlacesRepository {
+class PlacesRepositoryImpl @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+) : PlacesRepository {
+    override suspend fun getAllPlaces(
+        lonMin: Double,
+        lonMax: Double,
+        latMin: Double,
+        latMax: Double,
+        kinds: String,
+        limit: Int
+    ): Places {
+        return remoteDataSource.getAllPlaces(
+            lonMin = lonMin,
+            lonMax = lonMax,
+            latMin = latMin,
+            latMax = latMax,
+            kinds = kinds,
+            limit = limit
+        ).toEntity()
+    }
+
+    override suspend fun getNearestPlaces(lon: Double, lat: Double, radius: Int): Places {
+        return remoteDataSource.getNearestPlaces(
+            lon = lon,
+            lat = lat,
+            radius = radius,
+        ).toEntity()
+    }
+
+    override suspend fun getPlaceDetails(id: Double): PlaceDetails {
+        return remoteDataSource.getPlaceDetails(id = id).toEntity()
+    }
 }
--- app/src/main/java/com/androiddevtaha/touristspotter/usecase/PlacesRepository.kt
+++ app/src/main/java/com/androiddevtaha/touristspotter/usecase/PlacesRepository.kt
@@ -1,4 +1,24 @@
 package com.androiddevtaha.touristspotter.usecase
 
+import com.androiddevtaha.touristspotter.entites.placeDetails.PlaceDetails
+import com.androiddevtaha.touristspotter.entites.places.Places
+
 interface PlacesRepository {
+    suspend fun getAllPlaces(
+        lonMin: Double,
+        lonMax: Double,
+        latMin: Double,
+        latMax: Double,
+        kinds: String,
+        limit: Int,
+    ): Places
+
+    suspend fun getNearestPlaces(
+        lon: Double,
+        lat: Double,
+        radius: Int,
+    ): Places
+
+    suspend fun getPlaceDetails(id: Double): PlaceDetails
+
 }
